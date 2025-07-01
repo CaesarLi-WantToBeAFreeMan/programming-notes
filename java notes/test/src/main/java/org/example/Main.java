15ce26172bec9e614319f70231596c64565b0517
Caesar <krbw0110010000@gmail.com>
2025-07-01 18:06:17 +0800

    6:05 pm July 1, 2025 GMT+08:00

--- java notes/test/src/main/java/org/example/Main.java
+++ java notes/test/src/main/java/org/example/Main.java
@@ -1,39 +1,11 @@
 package org.example;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class Main{
     public static void main(String [] args){
-        //creation
-        OffsetDateTime now = OffsetDateTime.now();
-        System.out.println("now:\t" + now);
-        OffsetDateTime offset0 = OffsetDateTime.of(2024, 12, 25, 10, 30, 0, 0, ZoneOffset.ofHours(2));
-        System.out.printf(
-            "offset of offset0:\t%s\nyear of offset0:\t%d\nmonth of offset0:\t%s\nday of month of offset0:\t%d\nhour of offset0:\t%d\nminute of offset0:\t%d\nsecond of offset0:\t%d\nnanosecond of offset0:\t%d\n",
-            offset0.getOffset(),
-            offset0.getYear(),
-            offset0.getMonth(),
-            offset0.getDayOfMonth(),
-            offset0.getHour(),
-            offset0.getMinute(),
-            offset0.getSecond(),
-            offset0.getNano()
-        );
-        OffsetDateTime parsed = OffsetDateTime.parse("2025-01-01T12:00:00+02:00");
-        System.out.println("parsed:\t" + parsed);
-
-        //modifications
-        System.out.println("adding 84 hours:\t" + now.plus(5, ChronoUnit.HOURS).plusDays(3));
-
-        //comparisons
-        System.out.printf(
-            "is offset0 after now:\t%b\n",
-            offset0.isAfter(now)
-        );
-
-        //formatting
-        System.out.println("formatted offset0:\t" + offset0.format(DateTimeFormatter.ofPattern("MM dd, yy HH:mm:ss (Z)")));
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime.format(DateTimeFormatter.ofPattern("'MMM d, yyyy ''EEE'' hh:mm:ss a (z, VV)'")));
     }
 }
