5ef0e74fe42ea9b558bee9c0b0d4cf9711169728
Caesar James LEE <krbw0110010000@gmail.com>
2025-07-04 00:37:59 +0800

    12:37 am July 4, 2025 GMT+8

--- java notes/test/src/main/java/org/example/Main.java
+++ java notes/test/src/main/java/org/example/Main.java
@@ -1,10 +1,32 @@
 package org.example;
 
-interface greet{
-    void
-}
-public class Main{
-    public static void main(String [] args){
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static java.util.Collections.swap;
+
+public class Main {
+    public static void main(String[] args) {
+        List <Integer> nums = new ArrayList<>(Arrays.asList(1, -9, 4, 5, 0, -8));
+        System.out.println("original nums:\n");
+        printList(nums);
+        bubbleSort(nums);
+
+        System.out.println("sorted nums:\n");
+        printList(nums);
+    }
+
+    private static void bubbleSort(List <Integer> nums){
+        for(int i = 0; i < nums.size(); i++)
+            for(int j = i + 1; j < nums.size(); j++)
+                if(nums.get(i) > nums.get(j))
+                    swap(nums, i, j);
+    }
+
+    private static void printList(List <Integer> nums){
+        for(var num : nums)
+            System.out.print(num + "\t");
+        System.out.println();
     }
 }
