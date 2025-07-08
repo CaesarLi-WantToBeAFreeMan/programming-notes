fed0828e0665e1872b9737a011a2c7ff76ba5d89
Caesar <krbw0110010000@gmail.com>
2025-07-08 18:16:29 +0800

    6:16 pm July 8, 2025 GMT+8

--- java notes/test/src/main/java/org/example/Main.java
+++ java notes/test/src/main/java/org/example/Main.java
@@ -1,13 +1,61 @@
 package org.example;
 
-import java.util.function.Function;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main{
     public static void main(String [] args){
-        Function <String, Integer> stringToLength = words -> words.length();
-        Function <Integer, String> lengthToMessage = size -> "size:\t" + size;
-        Function <String, String> stringToMessage = stringToLength.andThen(lengthToMessage);
-        stringToMessage = stringToMessage.compose(words -> words.toUpperCase());
-        System.out.println(Function.identity().apply("Caesar") + "'s " + stringToMessage.apply("Caesar"));
+        List <String> fruits = Arrays.asList(
+            "apple", "banana", "grape", "pineapple", "coconut",
+            "strawberry", "blueberry", "watermelon", "pumpkin"
+        );
+
+        System.out.println(
+            "average length in the fruits:\t" +
+            fruits.stream().collect(Collectors.averagingInt(String::length))
+        );
+
+        System.out.println(
+            "summarizing length of fruits:\t" +
+            fruits.stream().collect(Collectors.summarizingInt(String::length))
+        );
+
+        System.out.println(
+            "count number of elements:\t" +
+            fruits.stream().collect(Collectors.counting())
+        );
+
+        System.out.println(
+            "group by the first letter in lexicographical order:\t" +
+            fruits.stream().collect(Collectors.groupingBy(fruit -> fruit.charAt(0)))
+        );
+
+        System.out.println(
+            "joined fruits with comma:\t" +
+            fruits.stream().collect(Collectors.joining(", "))
+        );
+
+        System.out.println(
+            "maximum length fruit:\t" +
+            fruits.stream().collect(Collectors.maxBy(Comparator.comparingInt(String::length))).stream().collect(Collectors.toSet()) + "\n" +
+            "minimum length fruit:\t" +
+            fruits.stream().collect(Collectors.minBy(Comparator.comparingInt(String::length))).stream().collect(Collectors.toSet())
+        );
+
+        System.out.println(
+            "add \" - \" between fruits:\t" +
+            fruits.stream().collect(Collectors.reducing(
+                (fruit0, fruit1) -> fruit0 + " - " + fruit1
+            )).orElse("single string")
+        );
+
+        System.out.println(
+            "\"false\" for fruits whose length are less than 5:\t" +
+            fruits.stream().collect(Collectors.partitioningBy(
+                fruit -> fruit.length() > 5
+            ))
+        );
     }
 }
